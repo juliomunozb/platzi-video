de50c3691ce7acce7d6c02d8c25d43a99e192cd1
Julio Muñoz <ing.juliomb@gmail.com>
2023-11-02 16:39:55 -0500

    agregando PropTypes a componente CarouselItem

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "../assets/styles/components/CarouselItem.scss";
 import iconPlay from "../assets/static/play-icon.png";
 import iconPlus from "../assets/static/plus-icon.png";
@@ -23,5 +24,13 @@ const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
     </div>
   </div>
 );
+
+CarouselItem.propTypes = {
+  cover: PropTypes.string,
+  title: PropTypes.string,
+  year: PropTypes.number,
+  contentRating: PropTypes.string,
+  duration: PropTypes.number,
+};
 
 export default CarouselItem;
