859087f08e53a5f61ce049193549140f543be8a5
Julio Muñoz <ing.juliomb@gmail.com>
2023-11-02 11:25:13 -0500

    Conectando con el API que trae la info de videos, usando useState y useEffect

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Search from "../components/Search";
 import Categories from "../components/Categories";
@@ -6,8 +6,26 @@ import Carousel from "../components/Carousel";
 import CarouselItem from "../components/CarouselItem";
 import Footer from "../components/Footer";
 import "../assets/styles/App.scss";
+const API = "http://localhost:3000/initalState";
 
 const App = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const videos = async () => {
+      try {
+        const response = await fetch(API);
+        const data = await response.json();
+        setVideos(data);
+      } catch (error) {
+        setVideos([]);
+        console.log("Error invocando API", error.message);
+      }
+    };
+    videos();
+  }, []);
+
+  console.log(videos);
   return (
     <div className="App">
       <Header />
